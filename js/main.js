182f5610156c61bb2262dd805643778fa2f467da
Akhmed8251 <bratya_250@mail.ru>
2023-11-21 17:20:35 +0300

    сделал страницы "вступай в сно" и "рейтинг", сделал адаптив для подвала и ссылков в шапке

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    new Swiper('.news__slider', {
-        direction: "vertical",
-        allowTouchMove: false,
-        effect: "cards",  
+    new Swiper('.news__slider', {    
         autoplay: {
             delay: 5000
         }, 
         loop: true,  
-        cardsEffect: {
-            perSlideOffset: 10,           
-            rotate: false,                   
-            slideShadows: true,            
-        },
         pagination: {                      
             el: '.swiper-pagination',
         },
         navigation: {                       
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: {
+                spaceBetween: 20
+            },
+            769: {
+                direction: "vertical",
+                allowTouchMove: false,
+                effect: "cards",
+                cardsEffect: {
+                    perSlideOffset: 10,           
+                    rotate: false,                   
+                    slideShadows: true,            
+                }
+            }
         }
     })
 })
